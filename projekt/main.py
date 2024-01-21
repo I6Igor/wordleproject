e435c5a50f4998fa2ad66d3496ec5725a3334b21
Igor Matyjaszczyk <igomatyjaszczyk@gmail.com>
2024-01-21 22:59:36 +0100

    wordle

--- projekt/main.py
+++ projekt/main.py
@@ -36,7 +36,18 @@ while TRY < 6:
         else:
             print("Not in words list")
 
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
+# Example usage:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(my_list)
+
+print("Sorted array:", my_list)
 
 #Tabela wyników
 print("\nSCORE: " + str(SCORE))
@@ -46,9 +57,8 @@ while 1>0:
         NAME = input("ENTER A 3 CHARACTER NAME TO ADD TO LEADERBOARD: ")
     else:
         break
+    SCORES = {NAME,SCORE}
+    print(SCORES)
 SCOREBOARD = open("scoreboard.txt","a")
 SCOREBOARD.write(str(SCORE) + ": " + NAME + "\n")
-scorelist = []
-for i in range(10):
-    scorelist.append(SCOREBOARD.read())
-print(SCOREBOARD.read())
+
